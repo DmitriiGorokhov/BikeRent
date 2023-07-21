b34d61fa424084b729d231214d4f1f1f0ea41751
Dmitrii Gorokhov <gordi2012@gmail.com>
2023-07-21 15:11:32 +0300

    Initial commit

---
+++ src/test/java/com/gorokhov/bikerent/BikeRentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gorokhov.bikerent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeRentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
